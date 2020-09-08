1f60a5b5e985093838ed1597fd69af9dd2b12e91
YanXu <2801500185@qq.com>
2020-09-08 09:46:28 +0800

    init

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,1 +1,7 @@
 import open3d as o3d
+
+import lib.datasets.synthia as synthia
+import lib.datasets.stanford as stanford
+import lib.datasets.scannet as scannet
+
+from lib.datasets import load_dataset
