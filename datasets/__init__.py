31c1add1f628dd1b20b97aea4589c7ab431d21ed
YanXu <2801500185@qq.com>
2020-09-08 10:27:09 +0800

    indent

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -21,10 +21,10 @@ def load_dataset(name):
     mdict = {dataset.__name__: dataset for dataset in DATASETS}
     if name not in mdict:
         print('Invalid dataset index. Options are:')
-    # Display a list of valid dataset names
-    for dataset in DATASETS:
-        print('\t* {}'.format(dataset.__name__))
-    raise ValueError(f'Dataset {name} not defined')
+        # Display a list of valid dataset names
+        for dataset in DATASETS:
+            print('\t* {}'.format(dataset.__name__))
+        raise ValueError(f'Dataset {name} not defined')
     DatasetClass = mdict[name]
 
     return DatasetClass
